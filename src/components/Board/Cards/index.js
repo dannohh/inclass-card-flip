d7e5991ed513a723f1ce4a584fcb218d39a68d23
dannohh <dtwomey13@gmail.com>
2020-08-12 13:11:01 -0500

    move card state to parent component

--- src/components/Board/Cards/index.js
+++ src/components/Board/Cards/index.js
@@ -3,25 +3,11 @@ import { Card } from './Card'
 import api from 'api'
 import './Cards.css'
 
-export const Cards = ({handler}) => {
+export const Cards = ({cards, handler}) => {
 
-const [cards, setCards] = useState([])
 const [flippedCards, setFlippedCards] = useState([])
 
-useEffect(() => {
-  (async () => {
-    const {cards} = await api.index(4)
-    const cardsWithDups = cards.concat(Array.from(cards))
 
-    const cardsWithIDs = cardsWithDups.map((card, i) => {
-      const cardCopy = JSON.parse(JSON.stringify(card))
-      card.id = `${card.code}-${i}`
-      return cardCopy
-    })
-
-    setCards(cardsWithIDs)
-  })()
-},[])
 
 const handleFlip = ({target: {dataset}}) => {
   if(!flippedCards.length) {
--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
 import {Cards} from './Cards'
+import api from 'api'
 
 export const Board = () => {
+
+  const [cards, setCards] = useState([])
+
+  useEffect(() => {
+    (async () => {
+      const {cards} = await api.index(4)
+      const cardsWithDups = cards.concat(Array.from(cards))
+
+      const cardsWithIDs = cardsWithDups.map((card, i) => {
+        const cardCopy = JSON.parse(JSON.stringify(card))
+        card.id = `${card.code}-${i}`
+        return cardCopy
+      })
+
+      setCards(cardsWithIDs)
+    })()
+  },[])
 return (
   <div>
-    <Cards  />
-
+    <Cards cards={cards}/>
   </div>
 )
 }
